7ca30dbc172c352283eb517448c63a985ad7511c
hiveben <ben@hivebp.io>
2023-02-14 13:53:06 +0100

    Atomicassets integration, redeem flow

--- WAX/contracts/redeem/include/RedeemProtocol.hpp
+++ WAX/contracts/redeem/include/RedeemProtocol.hpp
@@ -1,57 +1,85 @@
 #include <eosio/eosio.hpp>
 #include <eosio/singleton.hpp>
 #include <eosio/asset.hpp>
+#include "atomicdata.hpp"
 
 using namespace eosio;
 using namespace std;
+using namespace atomicdata;
 
-CONTRACT redeemprotocol : public contract
+CONTRACT redeemprtcol : public contract
 {
 public:
     using contract::contract;
 
     ACTION init();
 
-    ACTION mark(
-        name asset_owner,
-        uint64_t asset_id
-    );
-
-    ACTION transfer(
-        name asset_owner,
-        uint64_t asset_id
-    );
-
-    ACTION burn(
-        name asset_owner,
-        uint64_t asset_id
+    [[eosio::on_notify("atomicassets::transfer")]] void receive_asset_transfer(
+        name from,
+        name to,
+        vector<uint64_t> asset_ids,
+        string memo
     );
 
     ACTION settr(name new_token_receiver);
 
+    ACTION logredeem(
+        uint64_t asset_id,
+        uint64_t redemption_id,
+        name collection_name
+    );
+
+    ACTION accept(
+        name authorized_account,
+        name collection_name,
+        uint64_t asset_id
+    );
+
+    ACTION markpaid(
+        name asset_owner,
+        uint64_t asset_id
+    );
+
+    ACTION markshipped(
+        name authorized_account,
+        name collection_name,
+        uint64_t asset_id
+    );
+
+    ACTION markreceived(
+        name asset_owner,
+        uint64_t asset_id
+    );
+private:
     TABLE redemption_s
     {
         uint64_t asset_id;
         uint64_t redemption_id;
-        string method;
+        name asset_owner;
+        string status;
 
         uint64_t primary_key() const { return asset_id; }
     };
     typedef multi_index<"redemption"_n, redemption_s> redemption_t;
 
     // redemption_t redemptions;
-
     TABLE config_s
     {
         uint64_t redemption_counter = 1099511627776; // 2^40
         name token_receiver = name("waxchihkaiyu");
     };
+
     typedef singleton<name("config"), config_s> config_t;
-    // https://github.com/EOSIO/eosio.cdt/issues/280
     typedef multi_index<name("config"), config_s> config_t_for_abi;
 
+    void check_collection_auth(
+        name collection_name, 
+        name authorized_account
+    );
+
     config_t config = config_t(get_self(), get_self().value);
-    redemption_t get_redemption();
+
+    redemption_t redemptions = redemption_t(get_self(), get_self().value);
 
     struct assets_s {
         uint64_t         asset_id;
@@ -67,5 +95,41 @@ public:
     };
 
     typedef multi_index <name("assets"), assets_s> assets_t;
-    assets_t get_asset(name owner);
+
+    struct schemas_s {
+        name            schema_name;
+        vector <FORMAT> format;
+
+        uint64_t primary_key() const { return schema_name.value; }
+    };
+
+    typedef multi_index <name("schemas"), schemas_s> schemas_t;
+    
+    struct collections_s {
+        name             collection_name;
+        name             author;
+        bool             allow_notify;
+        vector <name>    authorized_accounts;
+        vector <name>    notify_accounts;
+        double           market_fee;
+        vector <uint8_t> serialized_data;
+
+        auto primary_key() const { return collection_name.value; };
+    };
+    
+    typedef eosio::multi_index<name("collections"), collections_s> collections_t;
+    
+    collections_t collections = collections_t(name("atomicassets"), name("atomicassets").value);
+
+    schemas_t get_schemas(name collection_name) {
+        return schemas_t(name("atomicassets"), collection_name.value);
+    }
+    
+    assets_t get_assets(name acc) {
+        return assets_t(name("atomicassets"), acc.value);
+    }
+    
+    redemption_t get_collection_redemptions(name collection_name) {
+        return redemption_t(get_self(), collection_name.value);
+    }
 };
--- WAX/contracts/redeem/src/RedeemProtocol.cpp
+++ WAX/contracts/redeem/src/RedeemProtocol.cpp
@@ -1,115 +1,193 @@
-#include <redeemprotocol.hpp>
+#include <RedeemProtocol.hpp>
 
-ACTION redeemprotocol::init() {
+ACTION redeemprtcol::init() {
     require_auth(get_self());
     config.get_or_create(get_self(), config_s{});
 }
 
-ACTION redeemprotocol::mark(
-    name asset_owner,
-    uint64_t asset_id)
-{
-    require_auth(asset_owner);
+void redeemprtcol::check_collection_auth(name collection_name, name authorized_account) {
+    require_auth(authorized_account);
 
-    auto a = get_asset(asset_owner);
-    auto aitr = a.find(asset_id);
-    check(aitr != a.end(),
-        "not asset owner");
+    auto collection_itr = collections.require_find(collection_name.value,
+        "No collection with this name exists");
 
+    check(std::find(
+        collection_itr->authorized_accounts.begin(),
+        collection_itr->authorized_accounts.end(),
+        authorized_account
+        ) != collection_itr->authorized_accounts.end(),
+        "Account is not authorized"
+    );
+}
 
-    auto r = get_redemption();
-    auto itr = r.find(asset_id);
-    check(itr == r.end(),
-        "This asset has been redeemed");
+ACTION redeemprtcol::accept(
+    name authorized_account,
+    name collection_name,
+    uint64_t asset_id
+) {
+    check_collection_auth(collection_name, authorized_account);
+
+    assets_t own_assets = get_assets(get_self());
+
+    auto asset_itr = own_assets.require_find(asset_id, ("Asset ID not found in contract: " + to_string(asset_id)).c_str());
+
+    redemption_t col_redemptions = get_collection_redemptions(asset_itr->collection_name);
+
+    auto redemption_itr = col_redemptions.require_find(asset_id, "No active Redemption for that Asset");
     
-    config_s current_config = config.get();
-    uint64_t redemption_id = current_config.redemption_counter++;
-    config.set(current_config, get_self());
-    
-    r.emplace( asset_owner, [&]( auto& row ) {
-        row.redemption_id = redemption_id;
-        row.asset_id = asset_id;
-        row.method = string("mark");
+    check(redemption_itr->status == "redeemed", "Redemption has already been accepted");
+
+    col_redemptions.modify(redemption_itr, get_self(), [&](auto& _redemption) {
+        _redemption.status = "accepted";
     });
 }
 
-ACTION redeemprotocol::transfer(
+ACTION redeemprtcol::markpaid(
     name asset_owner,
-    uint64_t asset_id)
-{
+    uint64_t asset_id
+) {
     require_auth(asset_owner);
 
-    auto r = get_redemption();
-    auto itr = r.find(asset_id);
-    check(itr == r.end(),
-        "This asset has been redeemed");
-    
-    config_s current_config = config.get();
-    uint64_t redemption_id = current_config.redemption_counter++;
-    name token_receiver = current_config.token_receiver;
-    config.set(current_config, get_self());
-    
-    r.emplace( asset_owner, [&]( auto& row ) {
-        row.redemption_id = redemption_id;
-        row.asset_id = asset_id;
-        row.method = string("transfer");
-    });
+    assets_t own_assets = get_assets(get_self());
 
-    vector<uint64_t> asset_ids{asset_id};
-    action(
-        permission_level{asset_owner, name("active")},
-        name("atomicassets"),
-        name("transfer"),
-        make_tuple(
-            asset_owner,
-            token_receiver,
-            asset_ids,
-            string("RE:DREAMER redemption"))
-    ).send();
+    auto asset_itr = own_assets.require_find(asset_id, ("Asset ID not found in contract: " + to_string(asset_id)).c_str());
+
+    redemption_t col_redemptions = get_collection_redemptions(asset_itr->collection_name);
+
+    auto redemption_itr = col_redemptions.require_find(asset_id, "No active Redemption for that Asset");
+
+    check(redemption_itr->asset_owner == asset_owner, "This is not your redemption process.");
+    
+    check(redemption_itr->status == "accepted", "Redemption is not accepted yet or already being processed");
+
+    col_redemptions.modify(redemption_itr, get_self(), [&](auto& _redemption) {
+        _redemption.status = "paid";
+    });
 }
 
-ACTION redeemprotocol::burn(
+ACTION redeemprtcol::markshipped(
+    name authorized_account,
+    name collection_name,
+    uint64_t asset_id
+) {
+    check_collection_auth(collection_name, authorized_account);
+
+    assets_t own_assets = get_assets(get_self());
+
+    auto asset_itr = own_assets.require_find(asset_id, ("Asset ID not found in contract: " + to_string(asset_id)).c_str());
+
+    redemption_t col_redemptions = get_collection_redemptions(asset_itr->collection_name);
+
+    auto redemption_itr = col_redemptions.require_find(asset_id, "No active Redemption for that Asset");
+    
+    check(redemption_itr->status == "paid", "Redemption is not paid or has already been processed");
+
+    col_redemptions.modify(redemption_itr, get_self(), [&](auto& _redemption) {
+        _redemption.status = "shipped";
+    });
+}
+
+ACTION redeemprtcol::markreceived(
     name asset_owner,
-    uint64_t asset_id)
-{
+    uint64_t asset_id
+) {
     require_auth(asset_owner);
 
-    auto r = get_redemption();
-    auto itr = r.find(asset_id);
-    check(itr == r.end(),
-        "This asset has been redeemed");
-    
-    config_s current_config = config.get();
-    uint64_t redemption_id = current_config.redemption_counter++;
-    config.set(current_config, get_self());
-    
-    r.emplace( asset_owner, [&]( auto& row ) {
-        row.redemption_id = redemption_id;
-        row.asset_id = asset_id;
-        row.method = string("burn");
-    });
+    assets_t own_assets = get_assets(get_self());
 
-    action(
-        permission_level{asset_owner, name("active")},
-        name("atomicassets"),
-        name("burnasset"),
-        make_tuple(
-            asset_owner,
-            asset_id)
-    ).send();
+    auto asset_itr = own_assets.require_find(asset_id, ("Asset ID not found in contract: " + to_string(asset_id)).c_str());
+
+    redemption_t col_redemptions = get_collection_redemptions(asset_itr->collection_name);
+
+    auto redemption_itr = col_redemptions.require_find(asset_id, "No active Redemption for that Asset");
+
+    check(redemption_itr->asset_owner == asset_owner, "This is not your redemption process.");
+
+    check(redemption_itr->status == "shipped", "Redemption is not shipped yet or already being processed");
+
+    col_redemptions.modify(redemption_itr, get_self(), [&](auto& _redemption) {
+        _redemption.status = "received";
+    });
 }
 
-ACTION redeemprotocol::settr(name new_token_receiver) {
+ACTION redeemprtcol::settr(name new_token_receiver) {
     require_auth(get_self());
     config_s current_config = config.get();
     current_config.token_receiver = new_token_receiver;
     config.set(current_config, get_self());
 }
 
-redeemprotocol::redemption_t redeemprotocol::get_redemption() {
-    return redemption_t(get_self(), get_self().value);
+ACTION redeemprtcol::logredeem(
+    uint64_t asset_id,
+    uint64_t redemption_id,
+    name collection_name
+) {
+    require_auth(get_self());
 }
 
-redeemprotocol::assets_t redeemprotocol::get_asset(name owner) {
-    return assets_t("atomicassets"_n, owner.value);
+void redeemprtcol::receive_asset_transfer(
+    name from,
+    name to,
+    vector<uint64_t> asset_ids,
+    string memo
+) {
+    if (to != get_self()) {
+        return;
+    }
+
+    check(memo.find("redeem") == 0, "Invalid Memo.");
+
+    assets_t own_assets = get_assets(get_self());
+
+    for (uint64_t asset_id : asset_ids) {
+        auto asset_itr = own_assets.require_find(asset_id, ("Asset ID not found in contract: " + to_string(asset_id)).c_str());
+
+        schemas_t collection_schemas = get_schemas(asset_itr->collection_name);
+        auto schema_itr = collection_schemas.find(asset_itr->schema_name.value);
+
+        ATTRIBUTE_MAP deserialized_immutable_data = deserialize(
+            asset_itr->immutable_serialized_data,
+            schema_itr->format
+        );
+
+        check(deserialized_immutable_data.find("redemption_type") != deserialized_immutable_data.end(),
+            ("Redemption Type not found in Asset Attributes" + to_string(asset_id)).c_str());
+
+        ATTRIBUTE_MAP deserialized_mutable_data = deserialize(
+            asset_itr->mutable_serialized_data,
+            schema_itr->format
+        );
+
+        check(deserialized_immutable_data.find("redemption_status") != deserialized_immutable_data.end(),
+            ("Redemption Status not found in Asset Attributes for Asset " + to_string(asset_id)).c_str());
+
+        auto redemption_itr = redemptions.find(asset_id);
+        check(redemption_itr == redemptions.end(), ("This asset is already being redeemed: " + to_string(asset_id)).c_str());
+        
+        config_s current_config = config.get();
+        uint64_t redemption_id = current_config.redemption_counter++;
+        name token_receiver = current_config.token_receiver;
+        config.set(current_config, get_self());
+
+        // Scope on Collection Name
+        redemption_t col_redemptions = get_collection_redemptions(asset_itr->collection_name);
+        
+        col_redemptions.emplace( get_self(), [&]( auto& row ) {
+            row.redemption_id = redemption_id;
+            row.asset_id = asset_id;
+            row.asset_owner = from;
+            row.status = "redeemed";
+        });
+
+        action(
+            permission_level{get_self(), name("active")},
+            get_self(),
+            name("logredeem"),
+            make_tuple(
+                asset_id,
+                redemption_id,
+                asset_itr->collection_name
+            )
+        ).send();
+    }
 }
